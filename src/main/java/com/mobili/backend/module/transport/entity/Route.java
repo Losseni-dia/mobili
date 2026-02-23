bed8830b5d36804ca6b07e44d35ee5bfe8438a10
Losseni Dia <losdiakite@gmail.com>
2026-02-23 06:53:31 +0100

    Route

---
+++ src/main/java/com/mobili/backend/module/transport/entity/Route.java
@@ -0,0 +1,33 @@
+package com.mobili.backend.module.transport.entity;
+
+import com.mobili.backend.shared.abstractEntity.AbstractEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "routes")
+@Getter
+@Setter
+public class Route extends AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne // Plusieurs trajets peuvent partir de la même ville
+    @JoinColumn(name = "departure_city_id", nullable = false)
+    private City departureCity;
+
+    @ManyToOne
+    @JoinColumn(name = "arrival_city_id", nullable = false)
+    private City arrivalCity;
+
+    private Double priceBase; // Prix indicatif
+}
