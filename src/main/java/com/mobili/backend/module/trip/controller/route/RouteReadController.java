f9fa54666469a83bdf524ad7b5f20d83b3881235
Losseni Dia <losdiakite@gmail.com>
2026-02-23 08:19:16 +0100

    route controllers

---
+++ src/main/java/com/mobili/backend/module/trip/controller/route/RouteReadController.java
@@ -0,0 +1,31 @@
+package com.mobili.backend.module.trip.controller.route;
+
+import com.mobili.backend.module.trip.dto.RouteDTO;
+import com.mobili.backend.module.trip.dto.mapper.RouteMapper;
+import com.mobili.backend.module.trip.service.RouteService;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/routes")
+@RequiredArgsConstructor
+public class RouteReadController {
+
+    private final RouteService routeService;
+    private final RouteMapper routeMapper = Mappers.getMapper(RouteMapper.class);
+
+    @GetMapping
+    public List<RouteDTO> getAll() {
+        return routeService.findAll().stream()
+                .map(routeMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public RouteDTO getById(@PathVariable Long id) {
+        return routeMapper.toDto(routeService.findById(id));
+    }
+}
