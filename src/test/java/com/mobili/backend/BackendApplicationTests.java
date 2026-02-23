7a76b84880646a920afc1285d4563b7fe3907869
Losseni Dia <losdiakite@gmail.com>
2026-02-23 06:44:25 +0100

    code rabbit

---
+++ src/test/java/com/mobili/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobili.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
