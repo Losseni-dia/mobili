669fe01730480afc6d49a5ecc578db9991d1afbc
Losseni Dia <losdiakite@gmail.com>
2026-02-23 06:51:54 +0100

    city

---
+++ src/main/java/com/mobili/backend/module/transport/entity/City.java
@@ -0,0 +1,28 @@
+package com.mobili.backend.module.transport.entity;
+
+import com.mobili.backend.shared.abstractEntity.AbstractEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cities")
+@Getter
+@Setter
+public class City extends AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String cityName;
+
+    @Column(nullable = false)
+    private String country;
+}
